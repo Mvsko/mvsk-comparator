1be69e74cc1cc7e930826cec8b0a5c31ca2c16a0
Mvsko <78796569+Mvsko@users.noreply.github.com>
2023-12-25 18:46:57 +0100

    Final setup
    Add version
    Add config program
    Modif index
    Delete packages

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const config = require('./settings/config.js');
-const pricefunding = require('./settings/getPrice.js');
 const hud = require ('./settings/hud.js');
+const ConsoleTitle = require('node-bash-title');
+const configjson = require('./settings/config.json');
+
+ConsoleTitle("Mvsk-Comparator - version " + configjson.comparator[0].version)
 
 config.loadWebsiteData();
 const getwebdata = config.getWebsiteData();
